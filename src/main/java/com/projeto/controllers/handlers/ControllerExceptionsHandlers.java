552acad08f42bbaae6adee65685b8ab90f40bec4
Jullianag <juh_g@hotmail.com>
2025-06-13 14:28:08 -0300

    customizando erros com CustomError, validationError e FieldMessage

--- src/main/java/com/projeto/controllers/handlers/ControllerExceptionsHandlers.java
+++ src/main/java/com/projeto/controllers/handlers/ControllerExceptionsHandlers.java
@@ -1,15 +1,22 @@
 package com.projeto.controllers.handlers;
 
+import com.projeto.dtos.CustomError;
 import com.projeto.services.exceptions.WebClientExceptions;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
 
 @RestControllerAdvice
 public class ControllerExceptionsHandlers {
 
     @ExceptionHandler(WebClientExceptions.class)
-    public ResponseEntity<String> handleWebClient(WebClientExceptions e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+    public ResponseEntity<CustomError> handleWebClient(WebClientExceptions e, ServerHttpRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getURI().getPath());
+        return ResponseEntity.status(status).body(err);
     }
 }
